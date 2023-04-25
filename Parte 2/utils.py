dd0913ba71289c20a393ca4c55db8f8fd9bf9f9f
Cesar Torres <catq2596@gmail.com>
2023-04-25 00:23:51 -0700

    Hyper parameter optimization for Tensorlfow Model

--- Parte 2/utils.py
+++ Parte 2/utils.py
@@ -3,13 +3,14 @@ import cv2
 import os
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+from sklearn.model_selection import train_test_split
 import random
 import tensorflow as tf
 
 
 class ProcessingUtils:
     @staticmethod
-    def load_dataset(path, classes, img_size=(224, 224), shuffle=True, verbose=True):
+    def load_dataset(path, classes, img_size=(224, 224), shuffle=True, seed=None, verbose=True):
         '''Load the dataset from the given path with folder for each class'''
         X = []
         y = []
@@ -22,28 +23,63 @@ class ProcessingUtils:
                 X.append(img)
                 y.append(i)
         if shuffle:
-            X, y = ProcessingUtils.shuffle_dataset(X, y)
+            if seed != None:
+                X, y = ProcessingUtils.shuffle_dataset(X, y, seed)
+            else:
+                X, y = ProcessingUtils.shuffle_dataset(X, y)
         return np.array(X), np.array(y)
 
     @staticmethod
-    def shuffle_dataset(X, y):
+    def shuffle_dataset(X, y, seed=None):
+        '''Shuffle the dataset'''
         c = list(zip(X, y))
-        random.shuffle(c)
+        if seed != None:
+            random.Random(seed).shuffle(c)
+        else:
+            random.shuffle(c)
         X, y = zip(*c)
         return np.array(X), np.array(y)
 
     @staticmethod
-    def plot_dataset(X, y, classes, n=10):
-        plt.figure(figsize=(20, 10))
-        for i in range(n):
-            plt.subplot(2, n, i + 1)
-            plt.imshow(X[i])
-            plt.title(classes[y[i]])
-            plt.axis('off')
+    def sample_dataset(X, y, class_dict, n=5):
+        '''Plot a sample of the datasetwith n images per class, default 5 images per class'''
+        fig, axs = plt.subplots(len(np.unique(y)), n, figsize=(15, 15))
+        for i, cls in enumerate(np.unique(y)):
+            for j in range(n):
+                img = cv2.cvtColor(X[y == cls][j], cv2.COLOR_BGR2RGB)
+                axs[i, j].imshow(img)
+                cls_name = class_dict[cls]
+                axs[i, j].set_title(cls_name)
+                axs[i, j].axis('off')
         plt.show()
 
     @staticmethod
-    def create_augmented_data_generator(X_train, y_train, batch_size=32):
+    def normalize_data(X):
+        '''Normalize the dataset'''
+        return X / 255.0
+
+    @staticmethod
+    def split_data(X, y, train_size, test_size, val_size=None, seed=None):
+        # Split data into train and test sets while maintaining class proportions
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y, train_size=train_size+val_size, test_size=test_size, stratify=y, random_state=seed)
+
+        # Split train set into train and validation sets if val_size is given
+        if val_size is not None:
+            X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, train_size=train_size/(
+                train_size+val_size), test_size=val_size/(train_size+val_size), stratify=y_train, random_state=seed)
+            return X_train, X_test, X_val, y_train, y_test, y_val
+        else:
+            return X_train, X_test, y_train, y_test
+
+    @staticmethod
+    def create_data_generator(X_train, y_train, batch_size=64):
+        datagen = tf.keras.preprocessing.image.ImageDataGenerator()
+        datagen.fit(X_train)
+        return datagen.flow(X_train, y_train, batch_size=batch_size)
+
+    @staticmethod
+    def create_augmented_data_generator(X_train, y_train, batch_size=64):
         datagen = tf.keras.preprocessing.image.ImageDataGenerator(
             rotation_range=20,
             width_shift_range=0.2,
